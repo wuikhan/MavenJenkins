275da0efee9dbb7f97298bff2a7347de6602d3ef
Waqas Khan <wuikhan@gmail.com>
2020-06-25 21:39:03 -0400

    made some changes

--- src/test/java/stepDefinition/LoginTest.java
+++ src/test/java/stepDefinition/LoginTest.java
@@ -14,7 +14,7 @@ public class LoginTest extends BaseClass {
 	
 	@Given("^I Open Chrome Browser$")
 	public void I_Open_Chrome_Browser() {
-		System.setProperty("webdriver.chrome.driver", "/Users/codegator/Desktop/drivers/chromedriver_83");
+		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"/Drivers/chromedriver");
 		driver = new ChromeDriver();
 		driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
